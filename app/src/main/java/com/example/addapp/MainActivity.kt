00be7648ccbf76f19f8e676da875585428f8d4f6
hamada456 <n87.takaya.hamada@gmail.com>
2021-11-25 15:03:32 +0900

    エラーメッセージを追加

--- app/src/main/java/com/example/addapp/MainActivity.kt
+++ app/src/main/java/com/example/addapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +21,23 @@ class MainActivity : AppCompatActivity() {
         var btnClear :Button = findViewById(R.id.btnClear)
         //クリック処理は.setOnClickListener
         btnAdd.setOnClickListener{
-            //Edit(編集型)は計算出来ない
-            //int型に直接変換出来ないのでString型にしてから
-            //.toStringでString型に変更できる　型推論で:Intは省略可能
-            var sum:Int = et1.text.toString().toInt() + et2.text.toString().toInt()
-            tvAns.text = "合計は" + sum
-        }
+            if( et1.text.toString() == "" || et2.text.toString() == ""){
+                //トースト
+                Toast.makeText(this , "数字を入力してください", Toast.LENGTH_LONG).show()
+                //アラート
+                AlertDialog.Builder(this)
+                    .setTitle("エラーです")
+                    .setMessage("数字を入力してください")
+                    .setPositiveButton("OK",null)
+                    .show()
+            } else {
+                //Edit(編集型)は計算出来ない
+                //int型に直接変換出来ないのでString型にしてから
+                //.toStringでString型に変更できる　型推論で:Intは省略可能
+                var sum: Int = et1.text.toString().toInt() + et2.text.toString().toInt()
+                tvAns.text = "合計は" + sum
+                    }
+            }
         //クリア処理
             btnClear.setOnClickListener {
                 et1.text.clear()
