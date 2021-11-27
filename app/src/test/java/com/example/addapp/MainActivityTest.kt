dcec2df01f20d0690a78221de0323fb4d3944005
hamada456 <n87.takaya.hamada@gmail.com>
2021-11-27 20:51:29 +0900

    テストコードファイル作成

---
+++ app/src/test/java/com/example/addapp/MainActivityTest.kt
@@ -0,0 +1,22 @@
+package com.example.addapp
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onCreate() {
+    }
+}
