0e35e1283250d6c93119f8af1fde1a89a66d5ceb
hamada456 <n87.takaya.hamada@gmail.com>
2021-12-02 14:19:23 +0900

    テストコード

--- app/src/main/java/com/example/addapp/MainActivity.kt
+++ app/src/main/java/com/example/addapp/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.addapp
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
-    override fun onCreate(savedInstanceState: Bundle?) {
+        @SuppressLint("SetTextI18n")
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
--- app/src/test/java/com/example/addapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/addapp/ExampleUnitTest.kt
@@ -1,15 +1,25 @@
 package com.example.addapp
 
+import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import com.example.addapp.MainActivity as MainActivity1
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-
+class ExampleUnitTest2 {
+    @Test
+    fun addAppの足し算テスト() {
+        var aaa = MainActivity1()
+        var actual = aaa.onCreate(2,2)
+        var tvAns1 = aaa.tvAns(4)
+        assertEquals(tvAns1,actual)
+    }
+}
 
 class ExampleUnitTest {
     @Test
